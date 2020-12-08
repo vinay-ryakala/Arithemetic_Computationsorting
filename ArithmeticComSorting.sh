40b01541edd32dfe5cec0b813d6f659c92272b9e
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-08 16:25:01 +0530

    Adding results to Dictionary

--- ArithmeticComSorting.sh
+++ ArithmeticComSorting.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to Arithmetic Computation and Sorting"
-#UC1
-read -p "give first value" a 
-read -p "give second value" b
-read -p "give third value" c
-#UC2
-Result1=$(($a+$b*$c))
-#UC3
-Result2=$(($a*$b+$c))
-#UC4
-Result3=$(($c+$a/$b))
-#UC5
-Result4=$(($a%$b+$c))
+
+declare -A resultsDictionary
+read -p "give a value" a 
+read -p "give b value" b
+read -p "give c value" c
+
+resultSDictionary["a+b*cResult1"]=$(($a+$b*$c))
+resultSDictionary["a*b+cResult2"]=$(($a*$b+$c))
+resultSDictionary["c+albResult3"]=$(($c+$a/$b))
+resultSDictionary["a%b+cResult4"]=$(($a%$b+$c))
+
