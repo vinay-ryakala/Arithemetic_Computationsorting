5f5eab440f0eed9de3f24cec93f2faa42371d3cf
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-08 18:02:07 +0530

    Refactoring code to read values from dictionary to array

--- ArithmeticComSorting.sh
+++ ArithmeticComSorting.sh
@@ -7,8 +7,16 @@ read -p "give a value" a
 read -p "give b value" b
 read -p "give c value" c
 
-resultSDictionary["a+b*cResult1"]=$(($a+$b*$c))
-resultSDictionary["a*b+cResult2"]=$(($a*$b+$c))
-resultSDictionary["c+albResult3"]=$(($c+$a/$b))
-resultSDictionary["a%b+cResult4"]=$(($a%$b+$c))
+resultsDictionary["a+b*cResult1"]=$(($a+$b*$c))
+resultsDictionary["a*b+cResult2"]=$(($a*$b+$c))
+resultsDictionary["c+albResult3"]=$(($c+$a/$b))
+resultsDictionary["a%b+cResult4"]=$(($a%$b+$c))
 
+arraykey=0
+for result in "${resultsDictionary[@]}"
+do
+	resultArray[$arrayKey]=$result
+	((arrayKey++))
+done
+
+echo "${resultArray[@]}"
