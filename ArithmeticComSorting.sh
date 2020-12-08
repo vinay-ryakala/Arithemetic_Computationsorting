05d8933f000cb686012565dd960ee182f81b6014
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-08 15:51:24 +0530

    Adding Computataion4 UC5

--- ArithmeticComSorting.sh
+++ ArithmeticComSorting.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome to Arithmetic Computation and Sorting"
-
+#UC1
 read -p "give first value" a 
 read -p "give second value" b
 read -p "give third value" c
 #UC2
 Result1=$(($a+$b*$c))
-#UC4
+#UC3
 Result2=$(($a*$b+$c))
-#UC5
+#UC4
 Result3=$(($c+$a/$b))
+#UC5
+Result4=$(($a%$b+$c))
